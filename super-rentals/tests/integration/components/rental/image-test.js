7b9d2a0c0ad25dd7ea68f6d1a2e30fbbcd19ee01
Ghaleb Hamadi <gah09@mail.aub.edu>
2021-02-18 13:12:30 +0200

    added tests for the toggle-size feature

--- super-rentals/tests/integration/components/rental/image-test.js
+++ super-rentals/tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function (hooks) {
@@ -20,5 +20,26 @@ module('Integration | Component | rental/image', function (hooks) {
       .exists()
       .hasAttribute('src', '/assets/images/teaching-tomster.png')
       .hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('clicking the component toggles its size', async function (assert) {
+    await render(hbs`
+    <Rental::Image
+      src="/assets/images/teaching-tomster.png"
+      alt="Teaching Tomster"
+    />
+  `);
+
+    assert.dom('button.image').exists();
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image').hasText('View larger');
+
+    await click('button.image');
+    assert.dom('.image').hasClass('large');
+    assert.dom('.image').hasText('View smaller');
+
+    await click('button.image');
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image').hasText('View larger');
   });
 });
